f42062d736e474b688c58b27fba2a7a562f63610
rlaubscher40-sys <rlaubscher40@gmail.com>
2026-09-07 20:58:48 +1000

    feat: route market intelligence explorer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -28,6 +28,7 @@ const DailyFeed = lazyWithReload(() => import("./pages/DailyFeed"), "DailyFeed")
 const AskDesk = lazyWithReload(() => import("./pages/AskDesk"), "AskDesk");
 const SharedBrief = lazyWithReload(() => import("./pages/SharedBrief"), "SharedBrief");
 const Signals = lazyWithReload(() => import("./pages/Signals"), "Signals");
+const Markets = lazyWithReload(() => import("./pages/Markets"), "Markets");
 const Editions = lazyWithReload(() => import("./pages/Editions"), "Editions");
 const ReadingQueue = lazyWithReload(() => import("./pages/ReadingQueue"), "ReadingQueue");
 const TopicThreads = lazyWithReload(() => import("./pages/TopicThreads"), "TopicThreads");
@@ -118,6 +119,7 @@ function Routes() {
         <Route path="/ask" component={AskDesk} />
         <Route path="/brief" component={SharedBrief} />
         <Route path="/signals" component={Signals} />
+        <Route path="/markets" component={Markets} />
         <Route path="/editions" component={Editions} />
         <Route path="/editions/:editionNumber" component={Editions} />
         <Route path="/queue" component={ReadingQueue} />
